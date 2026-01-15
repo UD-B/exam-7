fe2e90f94c5fdf70cce2aa6f36335b776e65053d
UD <yudibarak770@gmail.com>
2026-01-15 12:25:02 +0200

    third

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ItemsService } from './items.service';
 
 @Controller('items')
 export class ItemsController {
-    constructor(private itemsService: ItemsService) {}
+    constructor(private itemsService: ItemsService) { }
 
     @Post('transactions/purchase')
-    async purchase() {
-        
+    async purchase(
+        @Body('id') id: string,
+        @Body('name') name: string,
+        @Body('type') type: string,
+        @Body('quantity') quantity: number,
+        @Body('PricePerUnit') PricePerUnit: number,
+        @Body('hasImage') hasImage: boolean,
+    ) {
+        return this.itemsService.addPurchase({
+            id,
+            name,
+            type,
+            quantity,
+            PricePerUnit,
+            hasImage
+        })
     }
-
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Items } from './items.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ItemsService {}
+export class ItemsService {
+
+    constructor(
+        @InjectRepository(Items) private readonly itemsRepository: Repository<Items>,
+        private usersService: ItemsService
+    ) {
+    }
+    async addPurchase(data: any) {
+        return this.itemsRepository.save(data)
+    }
+}
